726a250d96534a0c7a212e60d48690673d91452a
ZanHaitao <99999966@outlook.com>
2020-07-03 22:15:10 +0800

    配置 VueRouter路由 与 Vuex状态管理

--- vue-meituan/src/main.js
+++ vue-meituan/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store/state'
+import router from './router/router'
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  store,
+  router,
 }).$mount('#app');
---
+++ vue-meituan/src/store/state.js
@@ -0,0 +1,20 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  state: {
+
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
