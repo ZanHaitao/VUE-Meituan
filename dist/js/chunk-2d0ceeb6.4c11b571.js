f89020fcbb60ca09b689ad848ebea0de6c6dd4f3
ZanHaitao <99999966@outlook.com>
2020-07-22 22:16:46 +0800

    打包文件

---
+++ dist/js/chunk-2d0ceeb6.4c11b571.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ceeb6"],{6204:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"page-blank"},[t("router-view")],1)},c=[],l=t("2877"),s={},u=Object(l["a"])(s,a,c,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0ceeb6.4c11b571.js.map
--- vue-meituan/src/router/router.js
+++ vue-meituan/src/router/router.js
@@ -53,7 +53,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   routes,
 });
 
